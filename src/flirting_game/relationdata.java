2444f6b743a5825c22824b20d9f554e784784453
Uxsx <snowlevlup@gmail.com>
2026-02-26 10:58:57 +0700

    fix ip

--- src/flirting_game/relationdata.java
+++ src/flirting_game/relationdata.java
@@ -3,5 +3,5 @@ package flirting_game;
 public class relationdata {
     public static relationship aliceRel = new relationship("อริส");
     public static boolean isOnlineMode = false;
-    public static String serverIP = "localhost"; 
+    public static String serverIP = "154.84.153.179"; 
 }
